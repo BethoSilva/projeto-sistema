fee40889c497acc7c434744a8db56957dfa3d51b
Humberto <54453477+BethoSilva@users.noreply.github.com>
2022-12-26 22:53:05 -0300

    Initial commit

---
+++ src/COMPONENTS/PAGES/Dashboard.js
@@ -0,0 +1,10 @@
+import './Pages.css';
+
+
+export default function Dashboard(){
+    return(
+        <div className='wrapper_page'>
+            <h1>Conteudo pagina Dashboard</h1>
+        </div>
+    )
+}
